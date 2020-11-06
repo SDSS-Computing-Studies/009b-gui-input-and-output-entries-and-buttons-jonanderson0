3158d580f76ec2464020cd55660a8764a0097b63
jonanderson0 <71406833+jonanderson0@users.noreply.github.com>
2020-11-06 12:04:42 -0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -12,3 +12,66 @@ The program should display the factored form in an Entry widget.
 Extension: make the + between a,b and b,c buttons that will toggle
 between + and -.
 """
+import tkinter as tk
+from tkinter import * 
+import math 
+
+
+
+
+win = tk.Tk()
+
+
+
+
+b = StringVar()
+b.set("Answer")
+
+
+def f():
+    b = e1.get()
+    b = float(b)
+    c = e2.get()
+    c = float(c)
+    d1 = float((b**2)- (4*c))
+    d1 = math.sqrt(d1)
+    d1 = (-1*b)+d1
+    d1 = d1/2
+    d2 = float((b**2)- (4*c))
+    d2 = math.sqrt(d2)
+    d2 = (-1*b)-d2
+    d2 = d2/2
+    equation = []
+    equation.append(d1)
+    equation.append(d2)
+    after.delete(0,END)
+    after.insert(0,equation)
+
+
+l1 = Label(win,text="fill in the coefficents for b and c")
+l2 = Label(win,text='click the "=" button and your factored equation will appear')
+l3 = Label(win,text="*note: a is equal to 1*")
+l4 = Label(win,text="")
+l5 = Label(win,text="x^2")
+l6 = Label(win,text="+")
+e1 = Entry(win,width=5)
+l7 = Label(win,text="x")
+l8 = Label(win,text="+")
+e2 = Entry(win,width=5)
+b1 = Button(win,text="=",command=f)
+after = Entry(win,width=20,textvariable=b)
+
+l1.grid(row=1,column=1)
+l2.grid(row=2,column=1)
+l3.grid(row=3,column=1)
+l3.grid(row=4,column=1)
+l5.grid(row=5,column=1,sticky = E)
+l6.grid(row=5,column=2)
+e1.grid(row=5,column=3)
+l7.grid(row=5,column=4)
+e2.grid(row=5,column=6)
+l8.grid(row=5,column=5)
+b1.grid(row=5,column=7)
+after.grid(row=5,column=8)
+
+win.mainloop()
